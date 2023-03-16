6d421c5eef800b9e37e05e92d0f96d32ddb274b1
Kevin Napier <kjnapier@umich.edu>
2023-03-16 18:46:54 -0400

    getting a feel

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,33 @@ impl Integrator for LeftRiemann {
         accum
     }
 }
+
+pub struct RightRiemann;
+
+impl Integrator for RightRiemann {
+    fn integrate<F>(&self, f: F, a: f64, b: f64, n_steps: usize) -> f64
+    where F: Fn(f64) -> f64 {
+        let step = (b - a) / n_steps as f64;
+        let mut accum = 0.0;
+        for i in 1..=n_steps {
+            accum += f(a + i as f64 * step) * step;    
+        }
+
+        accum
+    }
+}
+
+pub struct Midpoint;
+
+impl Integrator for Midpoint {
+    fn integrate<F>(&self, f: F, a: f64, b: f64, n_steps: usize) -> f64
+    where F: Fn(f64) -> f64 {
+        let step = (b - a) / n_steps as f64;
+        let mut accum = 0.0;
+        for i in 0..n_steps {
+            accum += f(a + (i as f64 + 0.5) * step) * step;    
+        }
+
+        accum
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![allow(unused)]
-use integrs::*;
+use integrs::{LeftRiemann, RightRiemann, Midpoint, Integrator};
 
 fn main() {
     let lr = LeftRiemann;
+    let rr = RightRiemann;
+    let mp = Midpoint;
+
 
     let result = lr.integrate(
         |x| x.sin(),
@@ -11,6 +14,22 @@ fn main() {
         10000,
     );
 
+    let result2 = rr.integrate(
+        |x| x.sin(),
+        0.0,
+        std::f64::consts::PI / 2.0,
+        10000,
+    );
+
+    let result3 = mp.integrate(
+        |x| x.sin(),
+        0.0,
+        std::f64::consts::PI / 2.0,
+        10000,
+    );
+
     println!("result: {result}");
+    println!("result2: {result2}");
+    println!("result3: {result3}");
 }
 
