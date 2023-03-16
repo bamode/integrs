969ef75c958d07ce39320c9e30860bfe48e83dcd
Brent Mode <brent.a.mode@gmail.com>
2023-03-16 17:21:56 -0500

    user specifies number of steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 pub trait Integrator {
-    fn integrate<F>(&self, f: F, a: f64, b: f64) -> f64
+    fn integrate<F>(&self, f: F, a: f64, b: f64, n_steps: usize) -> f64
     where F: Fn(f64) -> f64;
 }
 
 pub struct LeftRiemann;
 
 impl Integrator for LeftRiemann {
-    fn integrate<F>(&self, f: F, a: f64, b: f64) -> f64
+    fn integrate<F>(&self, f: F, a: f64, b: f64, n_steps: usize) -> f64
     where F: Fn(f64) -> f64 {
-        let step = (b - a) / 100.0;
+        let step = (b - a) / n_steps as f64;
         let mut accum = 0.0;
-        for i in 0..100 {
+        for i in 0..n_steps {
             accum += f(a + i as f64 * step) * step;    
         }
 
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ fn main() {
     let lr = LeftRiemann;
 
     let result = lr.integrate(
-        |x| (x + 1.).ln(),
+        |x| x.sin(),
         0.0,
-        1.0,
+        std::f64::consts::PI / 2.0,
+        10000,
     );
 
     println!("result: {result}");
