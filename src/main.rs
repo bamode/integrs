3ee2da3cef2be9248fd0aabc518b64f43b3298b4
Brent Mode <brent.a.mode@gmail.com>
2023-03-20 13:47:30 -0500

    a first pass attempt at a left Riemann integrator and a trait for 2D integration in rectangular regions.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
 #![allow(unused)]
-use integrs::{LeftRiemann, RightRiemann, Midpoint, Integrator};
+use integrs::{Integrator, LeftRiemann, Midpoint, RightRiemann};
 
 fn main() {
-
     let truth = 1.0;
 
     let scheme = LeftRiemann;
-    let result = scheme.integrate(
-        |x| x.sin(),
-        0.0,
-        std::f64::consts::PI / 2.0,
-        10000,
-    );
+    let result = scheme.integrate(|x| x.sin(), 0.0, std::f64::consts::PI / 2.0, 10000);
 
     let scheme = RightRiemann;
-    let result2 = scheme.integrate(
-        |x| x.sin(),
-        0.0,
-        std::f64::consts::PI / 2.0,
-        10000,
-    );
+    let result2 = scheme.integrate(|x| x.sin(), 0.0, std::f64::consts::PI / 2.0, 10000);
 
     let scheme = Midpoint;
-    let result3 = scheme.integrate(
-        |x| x.sin(),
-        0.0,
-        std::f64::consts::PI / 2.0,
-        10000,
+    let result3 = scheme.integrate(|x| x.sin(), 0.0, std::f64::consts::PI / 2.0, 10000);
+
+    println!(
+        "Right Riemann: {result}, residual: {residual}",
+        result = result,
+        residual = (result - truth).abs()
     );
-
-    println!("Right Riemann: {result}, residual: {residual}", result=result, residual=(result - truth).abs());
-    println!("Left Riemann: {result2}, residual: {residual}", result2=result2, residual=(result2 - truth).abs());
-    println!("Midpoint: {result3}, residual: {residual}", result3=result3, residual=(result3 - truth).abs());
-    
+    println!(
+        "Left Riemann: {result2}, residual: {residual}",
+        result2 = result2,
+        residual = (result2 - truth).abs()
+    );
+    println!(
+        "Midpoint: {result3}, residual: {residual}",
+        result3 = result3,
+        residual = (result3 - truth).abs()
+    );
 }
-
